3b194e85c81760cf39ad99f38a927d7f2212e29f
Dariusz Franczak <darius32@op.pl>
2018-09-05 23:27:10 +0100

    completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,3 +16,15 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+/*
+To use ngModel directive we need to import the module from the package which contains that module
+The ngModel directive is in FormsModule, so we need to import it by
+import { FormsModule } from '@angular/forms';
+
+Next we need to tel the Angular we want to include the FormsModule into our project by
+imports: [
+    BrowserModule,
+    FormsModule
+  ]
+*/
