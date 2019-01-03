1ab69c005fa9c4b4a572b8afee14b0b4ae5110c1
Dariusz Franczak <darius32@op.pl>
2019-01-03 23:37:34 +0000

    06 completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  inputText = 'Property Binding Text';
+  inputText2Way = '';
+  button = true;
+  showClick = '';
 
+  click(event) {
+    this.showClick = 'Button activated';
+    this.button = false;
+  }
 }
 
