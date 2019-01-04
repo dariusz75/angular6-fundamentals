9c10bffe3650d872ef2917caeb69b0cca62620fb
Dariusz Franczak <darius32@op.pl>
2019-01-04 13:02:05 +0000

    06c completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,35 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  inputText = '';
-  pasteWarning = '';
-  x = '';
-  y = '';
-  colorClass = 'color-1';
-  backgroundClass = '';
-  logoUrl = '';
+  name = '';
+  ocupation = '';
 
-  onFocus() {
-    this.backgroundClass = 'color-3';
+  changeName(event) {
+    this.name = event.target.value;
   }
 
-  onInput(event) {
-    this.inputText = event.target.value;
-  }
-
-  onMouseMove(event) {
-    this.x = event.clientX;
-    this.y = event.clientY;
-  }
-
-  onPaste() {
-    this.pasteWarning = 'Please do not paste';
-
-  }
-
-  change() {
-    this.inputText = 'Text changed';
-    this.colorClass = 'color-2';
+  changeOcupation(event) {
+    this.ocupation = event.target.value;
   }
 }
 
