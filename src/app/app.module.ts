e3c1f8b048064b1beb8539372a3c930918b11df8
Dariusz Franczak <darius32@op.pl>
2019-06-24 22:49:23 +0100

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-<<<<<<< HEAD
-
-=======
->>>>>>> 15-http-put
 import { AppComponent } from './app.component';
 import { HttpService } from './http.service';
 import { HttpClientModule } from '@angular/common/http';
@@ -16,12 +12,8 @@ import { HttpClientModule } from '@angular/common/http';
   ],
   imports: [
     BrowserModule,
-<<<<<<< HEAD
-    FormsModule
-=======
     FormsModule,
     HttpClientModule
->>>>>>> 15-http-put
   ],
   providers: [HttpService],
   bootstrap: [AppComponent]
